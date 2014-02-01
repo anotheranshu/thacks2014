0b61a17757135358be4908140d2ed9657f8bf527
anotheranshu <anotheranshu@gmail.com>
2014-02-01 13:21:50 -0500

    Got rid of the UTC module in calendar.  Let's hope this doesn't break anything.

--- sorude/schedule.py
+++ sorude/schedule.py
@@ -2,7 +2,7 @@ from pyquery import PyQuery as pq
 from auth import authenticate
 from datetime import datetime
 from urllib import urlencode
-from icalendar import Calendar, Event, UTC
+from icalendar import Calendar, Event
 import re
 import json
 
