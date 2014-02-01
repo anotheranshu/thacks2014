c5b9590630bf514a8a646638be5962aa350c1b24
Sally Gao <sallyg@andrew.cmu.edu>
2014-02-01 04:45:43 -0500

    Changed the helpers to include fb

--- sorude/templates/sorude/friendinfo.py
+++ sorude/templates/sorude/friendinfo.py
@@ -1,10 +1,3 @@
-from django.db import models
-from django.contrib.auth.models import User
-from sorude.models import *
-import datetime
-import random
-from django.utils import simplejson as json
-from django.conf import settings
 import facebook
 import json
 
@@ -18,16 +11,4 @@ def makeFriends(accessToken):
     return result
 
 
-def diff(a, b):
-  b = set(b)
-  return [aa for aa in a if aa not in b]
 
-def intersect(a, b):
-  b = set(b)
-  return [aa for aa in a if aa in b]
-
-#def interval_dates(times):
-#  for t in times:
-#    m = re.match(r"([A-Z]+)", t)
-#    m.group(0)
-    
