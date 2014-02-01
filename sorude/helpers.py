635e74cd7f4646a6e0dfec25a42ebf5ac267d8ad
anotheranshu <anotheranshu@gmail.com>
2014-01-31 22:57:28 -0500

    Added some models and helpers and stuff...

---
+++ sorude/helpers.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from sorude.models import *
+import datetime
+import random
+from django.utils import simplejson as json
+from django.conf import settings
+
+def diff(a, b):
+  b = set(b)
+  return [aa for aa in a if aa not in b]
+
+def intersect(a, b):
+  b = set(b)
+  return [aa for aa in a if aa in b]
+
+def interval_dates(times):
+  for t in times:
+    m = re.match(r"([A-Z]+)", t)
+    m.group(0)
+    
--- sorude/models.py
+++ sorude/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-  
+from django.utils import simplejson as json
+
+class Student(models.Model):
+  user = models.OneToOneField(User)
+  schedule = models.CharField(max_length=5000)
+  fb_id = models.IntegerField(default=0)
+  friend_list = models.CharField(max_length=5000)
+  andrew = models.CharField(max_length=100)
+  def is_free(self, time):
+    sched = json.loads(self.schedule)
+    free = True
+
+
+class Event(models.Model):
+  students = CharField(max_length=1000)
+  timestamp = models.DateTimeField()
+  accepts = CharField(max_length=1000)
+  ratings = CharField(max_length=1000)
