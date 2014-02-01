65477c8df951fe900513bf69f28bbd6cc36f867d
Arjun <arjunh@andrew.cmu.edu>
2014-02-01 09:03:54 -0500

    Added helper methods

--- sorude/helpers.py
+++ sorude/helpers.py
@@ -7,6 +7,8 @@ from django.utils import simplejson as json
 from django.conf import settings
 import facebook
 import json
+import math
+from random import choice
 
 #Given a user's access token, generates a JSON file of their friends
 def makeFriends(accessToken):
@@ -17,7 +19,6 @@ def makeFriends(accessToken):
     result = json.dumps(friendslist)
     return result
 
-
 def diff(a, b):
   b = set(b)
   return [aa for aa in a if aa not in b]
@@ -26,8 +27,58 @@ def intersect(a, b):
   b = set(b)
   return [aa for aa in a if aa in b]
 
-#def interval_dates(times):
-#  for t in times:
-#    m = re.match(r"([A-Z]+)", t)
-#    m.group(0)
-    
+def find_min_weight_friend(suggested_friends): 
+  min_weight = -1
+  min_weight_friend = null
+  for friend_suggestion in suggested_friends: 
+    if (min_weight == -1 or friend_suggestion['weight'] < min_weight): 
+      min_weight = friend_suggestion['weight']
+      min_weight_friend = friend_suggestion
+  return min_weight_friend
+
+def choose_top_friends(possible_friends): 
+  suggested_friends = []
+  max_friends = 3
+  # iterate through set of possible friends
+  for friend in possible_friends: 
+    friend_weight = friend['weight']
+    # if not reached max_friends, continue adding friends
+    if (len(suggested_friends) < max_friends): 
+      suggested_friends.append(friend)
+    else: 
+      # return min weight/min weight friend
+      min_weight_friend = find_min_weight_friend(suggested_friends)
+      # check if min weight less than current weight
+      if (min_weight_friend['weight'] < friend_weight): 
+        # remove min_weight_friend from list, add friend
+        suggested_friends.remove(min_weight_friend)
+        suggested_friends.add(friend)
+  return suggested_friends
+
+
+
+def suggestEvent(self, self_stime, self_etime): 
+  possible_friends = []
+  # check if free currently
+  if (is_free(self, self_stime, self_etime)): 
+    # get all people with same free timeslot
+    other_free_people = are_free(self_stime, self_etime)
+    # check to see which people have same classes as you
+    mutual_class_people = filtered_num_mutual_friends(self, other_free_people)
+    # for each person, calculate a weighted value (number common classes, 
+    # number mutual friends 
+    for student in other_free_people: 
+      student_obj = create_student_obj(student)
+      num_classes = student['num_classes']
+      num_friends = student['name']
+      weighted_val = num_friends + math.pow(num_classes, 3)
+      student_obj['weight'] = weighted_val
+      possible_friends.append(student_obj)
+    suggest_friends = choose_top_friends(possible_friends)
+    # choose highest weighted friends (1-3)
+    suggested_friends = choose_top_friends(possible_friends)
+    # return randomly choosen friend from list
+    return choice(suggested_friends)
+  else: 
+    # not currently free
+    return null
